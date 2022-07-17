83105db0676a2e2f08398c85a4e10d9d23e019ca
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-17 14:42:23 -0400

    updating files for freecodecamp blog project

--- main.js
+++ main.js
@@ -26,14 +26,24 @@ const toggleMenu = () => {
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchFormContainer = selectElement('#searh-form-container');
 
+formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove('activated'));
 // -- Close the search form popup on ESC keypress
-
+window.addEventListener('keyup', event => {
+    if(event.key === 'Escape') {
+        searchFormContainer.classList.remove('activated');
+    }
+})
 // Switch theme/add to local storage
 const bodyElement = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 
+// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
 if(currentTheme){
     bodyElement.classList.add('light-theme');
 
@@ -42,6 +52,7 @@ if(currentTheme){
 themeToggleBtn.addEventListener('click', () => {
     bodyElement.classList.toggle('light-theme');
 
+// If the body has the class of light theme then add it to local Storage, if not remove it
     if(bodyElement.classList.contains('light-theme')){
         localStorage.setItem('currentTheme', 'themeActive');
     }else{
@@ -49,3 +60,29 @@ themeToggleBtn.addEventListener('click', () => {
     }
 })
 // Swiper
+const swiper = new Swiper(".swiper", {
+    // How many slides to show
+    slidesPerView: 1,
+    // How much space between slides
+    spaceBetween: 20,
+    // Make the next and previous buttons work
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Make the pagination indicators work
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    //Responsive breakpoints for how many slides to show at that view
+    breakpoints: {
+        // 700px and up shoes 2 slides
+        700: {
+          slidesPerView: 2
+        },
+        // 1200px and up shoes 3 slides
+        1200: {
+            slidesPerView: 3
+        }
+    }   
+});
